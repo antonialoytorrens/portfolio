015d7c1c526a652cddbdd57df1baee4896554906
antonialoytorrens <antoni.aloytorrens@gmail.com>
2020-09-10 15:43:18 +0200

    First commit

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Project
+
+# Create your views here.
+class PortfolioList(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        projects = Project.objects.all()
+        context["projects"] = projects
+        return context
