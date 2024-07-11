acfa49ed11f48b99b603fdf85ce0e23aa3b51e90
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2024-07-11 16:20:45 +0200

    Format Python scripts using flake, set max length to 120

--- scripts/innerlinks.py
+++ scripts/innerlinks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# This is a list of common technologies and concepts (that have no translation) that I want to link automatically if some coincidence is met.
+# This is a list of common technologies and concepts (that have no translation)
+# that I want to link automatically if some coincidence is met.
 # For each and any markdown file.
 import re
 import os
@@ -30,14 +31,11 @@ innerlinks = {
     "Selenium": "https://www.selenium.dev",
     "Playwright": "https://playwright.dev",
     "Slim Framework": "https://www.slimframework.com/",
-
     # Operating Systems and Distributions (in alphabetic order)
     "Linux": "https://wikipedia.org/wiki/Linux_kernel",
-
     # Concepts (in alphabetic order)
     "CRUD": "https://wikipedia.org/wiki/CRUD",
     "framework": "https://wikipedia.org/wiki/Framework",
-
     # Programs (in alphabetic order)
     "Audacity": "https://www.audacityteam.org",
     "LMMS": "https://lmms.io",
@@ -48,23 +46,29 @@ innerlinks = {
 
 # Abbreviations or alternate names
 # Technologies
-innerlinks["CSS3"] = innerlinks["CSS"], # CSS specification
-innerlinks["Bootstrap4"] = f"{innerlinks['Bootstrap']}/docs/4.6/getting-started/introduction/", # Bootstrap 4 specification
-innerlinks["HTML5"] = innerlinks["HTML"] # HTML5 specification
-innerlinks["k8s"] = innerlinks["Kubernetes"] # Kubernetes abbr.
-innerlinks["JS"] = innerlinks["JavaScript"] # JavaScript abbr.
+innerlinks["CSS3"] = (innerlinks["CSS"],)  # CSS specification
+innerlinks["Bootstrap4"] = (
+    f"{innerlinks['Bootstrap']}/docs/4.6/getting-started/introduction/",
+)  # Bootstrap 4 spec
+innerlinks["HTML5"] = innerlinks["HTML"]  # HTML5 specification
+innerlinks["k8s"] = innerlinks["Kubernetes"]  # Kubernetes abbr.
+innerlinks["JS"] = innerlinks["JavaScript"]  # JavaScript abbr.
 
 # Programs
 # Some notes first:
 # * Beware of Sibelius, as it's also a known composer and can be wrongly linked and attributed to the program
-innerlinks["Sibelius"] = innerlinks["Sibelius7"] # Generic program
-innerlinks["Musescore3"] = innerlinks["Musescore"] # Versioned program
+innerlinks["Sibelius"] = innerlinks["Sibelius7"]  # Generic program
+innerlinks["Musescore3"] = innerlinks["Musescore"]  # Versioned program
 
 # * Note: if some link is rendered by error, add a !nolink next to the word and run the script again.
 # If "linked word" !nolink is read, remove !nolink and the associated link
-remove_nolinks_regex = re.compile(r'\[([^\]]+)\]\([^\)]+\)\s?!nolink')
-link_pattern = re.compile(r'\[[^\]]*\]\([^\)]+\)')  # Pattern for existing markdown links
-nolink_pattern = re.compile(r'\[([^\]]+)\]\([^\)]+\)\s?!nolink')  # Pattern for !nolink
+remove_nolinks_regex = re.compile(r"\[([^\]]+)\]\([^\)]+\)\s?!nolink")
+link_pattern = re.compile(
+    r"\[[^\]]*\]\([^\)]+\)"
+)  # Pattern for existing markdown links
+nolink_pattern = re.compile(r"\[([^\]]+)\]\([^\)]+\)\s?!nolink")  # Pattern for !nolink
+
+
 def apply_links(content, innerlinks):
     chunks = link_pattern.split(content)  # Split content by existing markdown links
     link_parts = link_pattern.findall(content)  # Get the existing markdown links
@@ -72,50 +76,57 @@ def apply_links(content, innerlinks):
     # Apply inner links only to the parts of content outside existing links
     for i in range(len(chunks)):
         for key, value in innerlinks.items():
-            chunks[i] = re.sub(r'\b' + re.escape(key) + r'\b', f'[{key}]({value})', chunks[i])
-    
+            chunks[i] = re.sub(
+                r"\b" + re.escape(key) + r"\b", f"[{key}]({value})", chunks[i]
+            )
+
     # Interleave the chunks and link_parts to get the final content
-    final_content = ''.join(val for pair in zip(chunks, link_parts + ['']) for val in pair)
+    final_content = "".join(
+        val for pair in zip(chunks, link_parts + [""]) for val in pair
+    )
 
     return final_content
 
+
 def remove_nolinks(content):
     # The regex pattern finds markdown links followed by !nolink and replaces it with just the linked word
-    content = remove_nolinks_regex.sub(r'\1', content)
+    content = remove_nolinks_regex.sub(r"\1", content)
     return content
 
+
 def apply_links_to_files(root_dir, innerlinks):
-    for filename in glob.iglob(root_dir + '/**/*.md', recursive=True):
+    for filename in glob.iglob(root_dir + "/**/*.md", recursive=True):
         # Do not modify files starting with underscore
-        if os.path.basename(filename).startswith('_'):
+        if os.path.basename(filename).startswith("_"):
             continue
 
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             lines = file.readlines()
 
         in_var_declaration = False
         for i, line in enumerate(lines):
             # Detect if we are inside a variable declaration block
-            if line.strip() == '+++':
+            if line.strip() == "+++":
                 in_var_declaration = not in_var_declaration
                 continue
 
             # Detect if we are inside a code declaration block
-            if line.strip() == '```':
+            if line.strip() == "```":
                 in_var_declaration = not in_var_declaration
                 continue
 
-        # If we are not in a variable declaration block, apply links
+            # If we are not in a variable declaration block, apply links
             if not in_var_declaration:
                 lines[i] = apply_links(line, innerlinks)
 
         # Join the lines back together and remove undesired links
         content = "".join(lines)
         final_content = remove_nolinks(content)
-        
+
         # Write back to the file
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             file.write(final_content)
+
 
 if __name__ == "__main__":
     # Usage
@@ -124,8 +135,11 @@ if __name__ == "__main__":
 
     # Remove content folder if exists, but avoid deleting by mistake
     if os.path.exists(root_dir) and os.path.isdir(root_dir):
-        confirm = input(f"The content folder exists. Are you sure you want to permanently delete the directory at {root_dir}? (y/N): ")
-        if confirm.lower() == 'y':
+        print("The content folder exists!")
+        confirm = input(
+            f"Are you sure you want to permanently delete the directory at {root_dir}? (y/N): "
+        )
+        if confirm.lower() == "y":
             shutil.rmtree(root_dir)
             print(f"Directory at {root_dir} has been deleted.")
         else:
